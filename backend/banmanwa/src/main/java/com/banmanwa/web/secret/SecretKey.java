7f893b31fb1862c97052a96775f4857e0bd97fc6
noyes5 <noyes5@naver.com>
2023-10-12 20:47:33 +0900

    feat: naver 로그인

--- backend/banmanwa/src/main/java/com/banmanwa/web/secret/SecretKey.java
+++ backend/banmanwa/src/main/java/com/banmanwa/web/secret/SecretKey.java
@@ -3,6 +3,8 @@ package com.banmanwa.web.secret;
 public class SecretKey {
 
     public static final String KAKAO_API_KEY = "cab1896fbf13780a270dddf0bcf5feb8";
+    public static final String NAVER_API_KEY = "B4CE5JEueBlwFSm2r8jw";
+    public static final String NAVER_CLIENT_SECRET = "mvhU97uwYK";
 
     private SecretKey() {
     }
